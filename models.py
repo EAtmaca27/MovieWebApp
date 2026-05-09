273ba0efd37ec6db7fd9b1b28da38428c7ede415
EAtmaca27 <enesatmaca27@googlemail.com>
2026-05-09 22:38:45 +0200

    User and Movie class added

---
+++ models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
